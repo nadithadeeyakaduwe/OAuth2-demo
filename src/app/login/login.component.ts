f7913f6dfb8ad3bd800c3e23f3ebd26d29f5ae8b
nadithadeeyakaduwe <ddn.harshana@gmail.com>
2018-10-11 20:01:08 +0530

    login is done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  isInvaliedCredintials: Boolean;
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.loginForm = new FormGroup({
+      username : new FormControl('', [Validators.required]),
+      password : new FormControl('', [Validators.required])
+    });
+    this.isInvaliedCredintials = false;
+  }
+
+  private login(username: String,  Password: String) {
+    if (username === 'SLIIT' && Password === '123') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public onSubmit() {
+    if (this.login(this.loginForm.get('username').value, this.loginForm.get('password').value)) {
+      this.isInvaliedCredintials = false;
+      this.router.navigate(['home']);
+    } else {
+      this.isInvaliedCredintials = true;
+    }
+  }
+
+  public reset() {
+    this.isInvaliedCredintials = false;
   }
 
 }
