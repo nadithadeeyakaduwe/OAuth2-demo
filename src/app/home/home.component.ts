71b8b707bf3e3104f86fa4d805ef768ad29231da
nadithadeeyakaduwe <ddn.harshana@gmail.com>
2018-10-13 21:40:00 +0530

    changes to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +11,6 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
 
 }
